d2ad577dac0c2909d2cf30341d01ac9a9e548ac5
Emilie LeMaoult <emilie.lemaoult@gmail.com>
2021-09-18 17:06:12 +0200

    Finishing Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -7,7 +8,37 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+
+    # no need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to '/tasks'
+  end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
